f37c739fb696a114c0ab05d7d842bb0334f92174
uoyniXnaH <b4baker@gmail.com>
2025-07-13 14:38:58 +0900

    CHORE: add themes

--- src/themes.ts
+++ src/themes.ts
@@ -1,9 +1,11 @@
 import { createTheme } from "@mui/material/styles";
 import { grey } from "@mui/material/colors";
+import { darkScrollbar } from "@mui/material";
 
 const themes = {
     light: createTheme({
         palette: {
+            mode: 'light',
             primary: {
                 main: grey[50],
                 contrastText: "#000"
@@ -12,10 +14,15 @@ const themes = {
                 main: grey[200],
                 contrastText: "#000"
             },
+            info: {
+                main: grey[300],
+                contrastText: "#666"
+            },
         }
     }),
     dark: createTheme({
         palette: {
+            mode: 'dark',
             primary: {
                 main: grey[900],
                 contrastText: "#fff"
@@ -24,7 +31,24 @@ const themes = {
                 main: "#363636",
                 contrastText: "#fff"
             },
-        }
+            info: {
+                main: grey[300],
+                contrastText: "#999"
+            },
+        },
+        components: {
+            MuiCssBaseline: {
+                styleOverrides: {
+                    body: {
+                        ...darkScrollbar({
+                            track: grey[900],
+                            thumb: grey[600],
+                            active: grey[500]
+                        }),
+                    },
+                },
+            },
+        },
     })
 }
 
