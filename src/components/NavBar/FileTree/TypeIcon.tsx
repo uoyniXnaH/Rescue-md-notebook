837f2371b724addf653bf2584bf921139deb1de1
uoyniXnaH <b4baker@gmail.com>
2025-07-19 22:57:56 +0900

    UPDATE: add opened folder icon

--- src/components/NavBar/FileTree/TypeIcon.tsx
+++ src/components/NavBar/FileTree/TypeIcon.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FolderIcon from "@mui/icons-material/Folder";
+import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import ArticleIcon from '@mui/icons-material/Article';
 
@@ -7,12 +8,14 @@ import { NodeType } from "../../../types";
 
 type Props = {
     fileType: NodeType;
+    isOpen?: boolean;
+    onClick?: (e: React.MouseEvent) => void;
 };
 
 const TypeIcon: React.FC<Props> = (props) => {
     switch (props.fileType) {
         case "folder":
-            return <FolderIcon />;
+            return (props.isOpen ? <FolderOpenIcon onClick={props.onClick} /> : <FolderIcon onClick={props.onClick} />);
         case "calendar":
             return <CalendarMonthIcon />;
         case "file":
