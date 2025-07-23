bfc1b32d26c733ae3036a4088dfe2a37c9f693ec
uoyniXnaH <b4baker@gmail.com>
2025-07-23 21:46:40 +0900

    UPDATE: use function to select theme

--- src/themes.ts
+++ src/themes.ts
@@ -2,70 +2,76 @@ import { createTheme } from "@mui/material/styles";
 import { grey } from "@mui/material/colors";
 import { darkScrollbar } from "@mui/material";
 
-const overrides = {
-    MuiCssBaseline: {
-        styleOverrides: {
-            body: {
-                ...darkScrollbar({
-                    track: grey[900],
-                    thumb: grey[600],
-                    active: grey[500]
-                }),
+const getOverrides = (theme: "light" | "dark") => {
+    const overrides = {
+        MuiCssBaseline: {
+            styleOverrides: {
+                body: theme == "dark" ?{
+                    ...darkScrollbar({
+                        track: grey[900],
+                        thumb: grey[600],
+                        active: grey[500]
+                    }),
+                } : {},
+                ul: {
+                    // listStyle: "none",
+                    paddingInlineStart: 0,
+                }
             },
-            ul: {
-                // listStyle: "none",
-                paddingInlineStart: 0,
+        },
+        MuiSvgIcon: {
+            styleOverrides: {
+                root: { verticalAlign: "middle" }
             }
-        },
-    },
-    MuiSvgIcon: {
-        styleOverrides: {
-            root: { verticalAlign: "middle" }
         }
-    }
+    };
+    return overrides;
 }
 
-const themes = {
-    light: createTheme({
-        palette: {
-            mode: 'light',
-            primary: {
-                main: grey[50],
-                contrastText: "#000"
+const selectTheme = (theme: "light" | "dark") => {
+    const themes = {
+        light: createTheme({
+            palette: {
+                mode: 'light',
+                primary: {
+                    main: grey[50],
+                    contrastText: "#000"
+                },
+                secondary: {
+                    main: grey[200],
+                    contrastText: "#000"
+                },
+                info: {
+                    main: grey[500],
+                    contrastText: "#666"
+                },
             },
-            secondary: {
-                main: grey[200],
-                contrastText: "#000"
+            components: {
+                ...getOverrides(theme)
+            }
+        }),
+        dark: createTheme({
+            palette: {
+                mode: 'dark',
+                primary: {
+                    main: grey[900],
+                    contrastText: "#fff"
+                },
+                secondary: {
+                    main: "#363636",
+                    contrastText: "#fff"
+                },
+                info: {
+                    main: grey[300],
+                    contrastText: "#999"
+                }
             },
-            info: {
-                main: grey[500],
-                contrastText: "#666"
-            },
-        },
-        components: {
-            ...overrides,
-        }
-    }),
-    dark: createTheme({
-        palette: {
-            mode: 'dark',
-            primary: {
-                main: grey[900],
-                contrastText: "#fff"
-            },
-            secondary: {
-                main: "#363636",
-                contrastText: "#fff"
-            },
-            info: {
-                main: grey[300],
-                contrastText: "#999"
-            },
-        },
-        components: {
-            ...overrides,
-        }
-    })
+            components: {
+                ...getOverrides(theme)
+            }
+        })
+    };
+    return themes[theme];
 }
 
-export default themes;
+export default selectTheme;
