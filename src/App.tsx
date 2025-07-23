cf29cff9770c74b8c125537231d8a676dbe52e64
uoyniXnaH <b4baker@gmail.com>
2025-07-23 21:47:49 +0900

    UPDATE: use zustand, new theme selector, and i18n in app entrance

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { useEffect } from "react";
 import { Stack, Box, CssBaseline } from "@mui/material";
 import {ThemeProvider} from "@mui/material/styles";
 
-import themes from "./themes";
+import selectTheme from "./themes";
 import NavBar from "./components/NavBar/NavBar";
 import EditArea from "./components/EditArea";
 import ViewArea from "./components/ViewArea";
+import { useTranslation } from "react-i18next";
+import { useSettingStore } from "./store";
 
 function App() {
+  const theme = useSettingStore((state) => state.theme);
+  const language = useSettingStore((state) => state.language);
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    i18n.changeLanguage(language);
+  }, [language]);
+
   return (
-    <ThemeProvider theme={themes.dark}>
+    <ThemeProvider theme={selectTheme(theme)}>
       <CssBaseline enableColorScheme />
       <Box width="100%" height="100vh">
         <Stack direction="row" height="100%">
