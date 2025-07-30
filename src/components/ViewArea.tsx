ee0bf152eaa2a3cf1eecf60ab6ba2333938fdc23
uoyniXnaH <b4baker@gmail.com>
2025-07-30 22:18:56 +0900

    FEAT: add view area basic layout

--- src/components/ViewArea.tsx
+++ src/components/ViewArea.tsx
@@ -1,9 +1,24 @@
 import { Box } from "@mui/material";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+
+import BlankPage from "./BlankPage";
+import { useDisplayStore } from "../store";
 
 function ViewArea() {
+  const currentFilePath = useDisplayStore((state) => state.currentFilePath);
+  const currentFileContents = useDisplayStore((state) => state.currentFileContents);
+ 
   return (
-    <Box width="41%" maxWidth={788} sx={{ bgcolor: "primary.main", color: "primary.contrastText" }}>
-      <h1>Rescue Notebook</h1>
+    <Box width="41%" maxWidth={788} px={1.5} className="markdown-body">
+      {currentFilePath ? (
+        <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[[rehypeHighlight, {detect: true, ignoreMissing: true}]]}>
+          {currentFileContents}
+        </ReactMarkdown>
+      ) : (
+        <BlankPage />
+      )}
     </Box>
   );
 }
