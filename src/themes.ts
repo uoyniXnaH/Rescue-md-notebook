b09cbb39e25d5ca2607d93246364c33520c71da9
uoyniXnaH <b4baker@gmail.com>
2025-07-30 22:16:18 +0900

    FEAT: add style for markdown, except for code syntax highlight and checkbox

--- src/themes.ts
+++ src/themes.ts
@@ -1,6 +1,37 @@
 import { createTheme } from "@mui/material/styles";
-import { grey } from "@mui/material/colors";
+import { grey, green } from "@mui/material/colors";
 import { darkScrollbar } from "@mui/material";
+
+const palettes = {
+    light: {
+        primary: {
+            main: grey[50],
+            contrastText: "#000"
+        },
+        secondary: {
+            main: grey[200],
+            contrastText: "#000"
+        },
+        info: {
+            main: grey[500],
+            contrastText: "#666"
+        },
+    },
+    dark: {
+        primary: {
+            main: grey[900],
+            contrastText: "#fff"
+        },
+        secondary: {
+            main: "#363636",
+            contrastText: "#fff"
+        },
+        info: {
+            main: grey[300],
+            contrastText: "#999"
+        }
+    }
+}
 
 export const selectTheme = (theme: "light" | "dark") => {
     const overrides = {
@@ -12,7 +43,78 @@ export const selectTheme = (theme: "light" | "dark") => {
                         thumb: grey[600],
                         active: grey[500]
                     }),
-                } : {}
+                } : {},
+                ".markdown-body": {
+                    backgroundColor: palettes[theme].primary.main,
+                    h1: {
+                        borderBottom: `1px solid ${palettes[theme].secondary.main}`,
+                    },
+                    h2: {
+                        borderBottom: `1px solid ${palettes[theme].secondary.main}`,
+                    },
+                    ul: {
+                        paddingInlineStart: 24
+                    },
+                    "ul.contains-task-list": {
+                        listStyle: "none",
+                        paddingInlineStart: 0,
+                    },
+                    "input.task-list-item": {
+                        bgColor: green[500]
+                    },
+                    table: {
+                        margin: 12,
+                        borderSpacing: 0,
+                        borderCollapse: "collapse"
+                    },
+                    "tr:nth-child(2n)": {
+                        backgroundColor: palettes[theme].secondary.main,
+                    },
+                    th: {
+                        backgroundColor: palettes[theme].secondary.main,
+                    },
+                    "th, td": {
+                        padding: "6px 12px",
+                        borderWidth: "1px",
+                        borderStyle: "solid",
+                        borderColor: palettes[theme].secondary.main
+                    },
+                    pre: {
+                        padding: "16px 12px",
+                        backgroundColor: palettes[theme].secondary.main,
+                        borderRadius: 4,
+                        overflowX: "auto",
+                        fontSize: "0.9rem",
+                        code: {
+                            padding: 0,
+                            border: "none",
+                            backgroundColor: "transparent",
+                            fontSize: "inherit",
+                            whiteSpace: "pre-wrap",
+                            wordBreak: "break-all",
+                        }
+                    },
+                    code: {
+                        display: "inline-block",
+                        padding: "1px 4px",
+                        margin: 1,
+                        fontSize: "80%",
+                        backgroundColor: palettes[theme].secondary.main,
+                        borderRadius: 4,
+                    },
+                    blockquote: {
+                        margin: "0 0 1em",
+                        paddingLeft: 10,
+                        borderLeft: `4px solid ${palettes[theme].info.contrastText}`,
+                        color: palettes[theme].info.contrastText,
+                        blockquote: {
+                            marginY: 12
+                        },
+                        p: {
+                            margin: 0
+                        }
+                    },
+                },
             },
         },
         MuiSvgIcon: {
@@ -26,18 +128,7 @@ export const selectTheme = (theme: "light" | "dark") => {
         light: createTheme({
             palette: {
                 mode: 'light',
-                primary: {
-                    main: grey[50],
-                    contrastText: "#000"
-                },
-                secondary: {
-                    main: grey[200],
-                    contrastText: "#000"
-                },
-                info: {
-                    main: grey[500],
-                    contrastText: "#666"
-                },
+                ...palettes.light
             },
             components: {
                 ...overrides
@@ -46,18 +137,7 @@ export const selectTheme = (theme: "light" | "dark") => {
         dark: createTheme({
             palette: {
                 mode: 'dark',
-                primary: {
-                    main: grey[900],
-                    contrastText: "#fff"
-                },
-                secondary: {
-                    main: "#363636",
-                    contrastText: "#fff"
-                },
-                info: {
-                    main: grey[300],
-                    contrastText: "#999"
-                }
+                ...palettes.dark
             },
             components: {
                 ...overrides
