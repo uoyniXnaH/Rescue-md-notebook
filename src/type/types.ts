8d1616a80392f51bd49858b15b44ea6c827988ca
uoyniXnaH <b4baker@gmail.com>
2025-08-03 11:01:10 +0900

    STYLE: adjust type ts indent size

--- src/type/types.ts
+++ src/type/types.ts
@@ -1,28 +1,28 @@
 export type NodeEnum = "file" | "folder" | "calendar";
 export type NodeData = {
-  fileType: NodeEnum;
-  isOpen?: boolean;
-  filePath: string;
+    fileType: NodeEnum;
+    isOpen?: boolean;
+    filePath: string;
 };
 
 export type themeEnum = "light" | "dark";
 export type languageEnum = "en" | "sc" | "ja";
 export type settingStoreType = {
-  theme: themeEnum;
-  language: languageEnum;
-  setTheme: (theme: themeEnum) => void;
-  setLanguage: (language: languageEnum) => void;
+    theme: themeEnum;
+    language: languageEnum;
+    setTheme: (theme: themeEnum) => void;
+    setLanguage: (language: languageEnum) => void;
 }
 
 export type displayStoreType = {
-  isNavBarShown: boolean;
-  isEditAreaShown: boolean;
-  isSettingShown: boolean;
-  currentFilePath: string | undefined;
-  currentFileContents: string | undefined;
-  setIsNavBarShown: (isShown: boolean) => void;
-  setIsEditAreaShown: (isShown: boolean) => void;
-  setIsSettingShown: (isShown: boolean) => void;
-  setCurrentFilePath: (filePath: string) => void;
-  setCurrentFileContents: (contents: string) => void;
+    isNavBarShown: boolean;
+    isEditAreaShown: boolean;
+    isSettingShown: boolean;
+    currentFilePath: string | undefined;
+    currentFileContents: string | undefined;
+    setIsNavBarShown: (isShown: boolean) => void;
+    setIsEditAreaShown: (isShown: boolean) => void;
+    setIsSettingShown: (isShown: boolean) => void;
+    setCurrentFilePath: (filePath: string) => void;
+    setCurrentFileContents: (contents: string) => void;
 }
