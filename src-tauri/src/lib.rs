f7328240b27897b50aa6f44ebf01416b25d4a8a2
uoyniXnaH <b4baker@gmail.com>
2025-12-14 17:37:37 +0900

    CHORE: add tauri dialog

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ fn test_command() -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
             test_command,
             get_gconfig_path,
