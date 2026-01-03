2422cddff9e530d4c732cf165e78c07f42d5ea2f
uoyniXnaH <b4baker@gmail.com>
2026-01-03 16:31:44 +0900

    FEAT: add message modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,20 +1,36 @@
 import * as React from 'react';
 import { ThemeProvider } from "@mui/material/styles";
-import { Stack, Box, Typography, Modal, Button } from '@mui/material';
+import { Stack, Box, Typography, Modal, Button, IconButton, Fade } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 import { selectTheme } from "../themes";
 import { useSettingStore } from "@store/store";
 
 const style = {
     position: 'absolute',
+    bgcolor: 'secondary.main',
+};
+
+const basicModalStyle = {
+    ...style,
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
-    bgcolor: 'secondary.main',
     borderRadius: 4,
     p: 4,
-};
+}
+
+const messageModalStyle = {
+    ...style,
+    bottom: 0,
+    right: 0,
+    width: 240,
+    pt: 3,
+    pb: 2,
+    pl: 2,
+    pr: 4
+}
 
 interface ModalOptions {
     contents: Array<string> | string;
@@ -37,7 +53,7 @@ const BasicModal: React.FC<ModalProps> = ({ open, onClose, contents, leftButtonT
             open={open}
             onClose={onClose}
         >
-            <Box sx={style}>
+            <Box sx={basicModalStyle}>
                 {Array.isArray(contents) ? (
                     contents.map((line, index) => (
                         <Typography key={index} sx={{ mt: 2 }}>
@@ -58,38 +74,94 @@ const BasicModal: React.FC<ModalProps> = ({ open, onClose, contents, leftButtonT
     );
 }
 
+const MessageModal: React.FC<ModalProps> = ({ open, onClose, contents }) => {
+    const settings = useSettingStore((state) => state.settings);
+    
+    setTimeout(() => {
+        if (open) {
+            onClose();
+        }
+    }, 5000);
+
+    return (
+        <ThemeProvider theme={selectTheme(settings.color_mode)}>
+            <Fade in={open} timeout={800}><Box sx={{ ...messageModalStyle, display: open ? 'block' : 'none' }}>
+                {Array.isArray(contents) ? (
+                    contents.map((line, index) => (
+                        <Typography variant="caption" key={index} sx={{ mt: 2 }}>
+                            {line}
+                        </Typography>
+                    ))
+                ) : (
+                    <Typography variant="caption" sx={{ mt: 2 }}>
+                        {contents}
+                    </Typography>
+                )}
+                <IconButton
+                    aria-label="close"
+                    onClick={onClose}
+                    size="small"
+                    sx={{
+                        position: 'absolute',
+                        right: 4,
+                        top: 4,
+                        color: 'secondary.contrastText',
+                    }}
+                >
+                    <CloseIcon fontSize="small" />
+                </IconButton>
+            </Box></Fade>
+        </ThemeProvider>
+    );
+}
+
 type ModalContextType = {
-    showModal: (options: ModalOptions) => void;
+    showBasicModal: (options: ModalOptions) => void;
+    showMessageModal: (options: ModalOptions) => void;
     closeModal: () => void;
 };
 
 const ModalContext = React.createContext<ModalContextType | undefined>(undefined);
 
 const ModalProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [open, setOpen] = React.useState(false);
+    const [openBasic, setOpenBasic] = React.useState(false);
+    const [openMessage, setOpenMessage] = React.useState(false);
     const [opts, setOpts] = React.useState<ModalOptions>({ contents: "" });
 
-    const showModal = (options: ModalOptions) => {
+    const showBasicModal = (options: ModalOptions) => {
         setOpts(options);
-        setOpen(true);
+        setOpenBasic(true);
+        setOpenMessage(false);
+    }
+
+    const showMessageModal = (options: ModalOptions) => {
+        setOpts(options);
+        setOpenMessage(true);
+        setOpenBasic(false);
     }
 
     const closeModal = () => {
-        setOpen(false);
+        setOpenBasic(false);
+        setOpenMessage(false);
         if (opts.onLeftButtonClick) opts.onLeftButtonClick();
     }
 
     return (
-        <ModalContext.Provider value={{ showModal, closeModal }}>
+        <ModalContext.Provider value={{ showBasicModal, showMessageModal, closeModal }}>
             {children}
             <BasicModal
-                open={open}
+                open={openBasic}
                 onClose={closeModal}
                 contents={opts.contents}
                 leftButtonText={opts.leftButtonText}
                 rightButtonText={opts.rightButtonText}
                 onLeftButtonClick={opts.onLeftButtonClick}
                 onRightButtonClick={opts.onRightButtonClick}
+            />
+            <MessageModal
+                open={openMessage}
+                onClose={closeModal}
+                contents={opts.contents}
             />
         </ModalContext.Provider>
     )
