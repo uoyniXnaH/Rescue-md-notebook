e5306d3d3ff73113ec902771d9b2eaae0b28c016
uoyniXnaH <b4baker@gmail.com>
2026-01-10 14:08:02 +0900

    UPDATE: set edit field rows with window height for better UX

--- src/components/EditArea/EditArea.tsx
+++ src/components/EditArea/EditArea.tsx
@@ -5,6 +5,7 @@ import Title from "./Title";
 import BlankPage from "../BlankPage";
 import { useDisplayStore, useFileTreeStore, useFocusStore } from "@store/store";
 import { useTranslation } from "react-i18next";
+import { useWindowSize } from "@src/hooks";
 
 function EditArea() {
   const currentFileContents = useDisplayStore((state) => state.currentFileContents);
@@ -12,6 +13,7 @@ function EditArea() {
   const setIsChanged = useDisplayStore((state) => state.setIsChanged);
   const selectedNodeId = useFileTreeStore((state) => state.selectedNodeId);
   const setFocusArea = useFocusStore((state) => state.setFocusArea);
+  const { height } = useWindowSize();
   const { t } = useTranslation();
 
   const onChangeText = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -24,14 +26,14 @@ function EditArea() {
       {selectedNodeId ? (
         <>
           <Title />
-          <Box height="93%" overflow="auto" bgcolor="primary.main" pb={40} borderRadius={1}>
+          <Box height="93%" overflow="auto" bgcolor="primary.main" borderRadius={1}>
             <TextField
               multiline
               fullWidth
               focused
               variant="outlined"
               placeholder={t("edit.empty_file_prompt")}
-              // rows={35}
+              rows={Math.floor(height * 0.93 / 24)}
               value={currentFileContents}
               onChange={(e) => onChangeText(e)}
               // sx={{ borderRadius: 1, bgcolor: "primary.main" }}
