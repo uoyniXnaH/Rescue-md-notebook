5854350641aa6fa9c98cdfc1d1e1ca44e953f28f
uoyniXnaH <b4baker@gmail.com>
2026-02-03 22:13:37 +0900

    UPDATE: improved title display

--- src/components/EditArea/Title.tsx
+++ src/components/EditArea/Title.tsx
@@ -3,10 +3,11 @@ import { Box, Stack, IconButton, Typography, Popper } from "@mui/material";
 import SaveIcon from '@mui/icons-material/Save';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+import dayjs from "dayjs";
 
 import RsnCalendar from "./Calendar/RsnCalendar";
 import { useDisplayStore, useFileTreeStore } from "@store/store";
-import { useFileActions } from "@src/hooks";
+import { useFileActions, useWindowSize } from "@src/hooks";
 import useTauriCmd from "@tauri/TauriCmd";
 
 function Title() {
@@ -14,7 +15,9 @@ function Title() {
   const isChanged = useDisplayStore((state) => state.isChanged);
   const selectedNodeId = useFileTreeStore((state) => state.selectedNodeId);
   const fileTreeData = useFileTreeStore((state) => state.fileTreeData);
+  const selectedDate = useFileTreeStore((state) => state.selectedDate);
   const { saveFile } = useFileActions();
+  const { width } = useWindowSize();
   const { getNodeById } = useTauriCmd();
   const [filename, setFilename] = React.useState<string>("");
   const [calendarAnchorEl, setCalendarAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -44,7 +47,8 @@ function Title() {
     <Box pt={1.5} sx={{ display: 'flex', alignItems: 'center', color: 'primary.contrastText' }}>
       <Stack direction="row" spacing={1} justifyContent="space-between" alignItems="center" width="100%">
         <Stack direction="row" spacing={1} alignItems="center">
-          <Typography variant="body1">{filename}</Typography>
+          <Typography variant="body1" noWrap maxWidth={width * 0.25}>{filename}</Typography>
+          <Typography variant="body1">{selectedDate ? ` - ${dayjs(selectedDate).format("YY/MM/DD")}` : ""}</Typography>
           <IconButton color="inherit" onClick={handleSave} disabled={!isChanged}>
             <SaveIcon />
           </IconButton>
