3f5cd9788b4019d6f4a82a2c568f8a84ac70a071
uoyniXnaH <b4baker@gmail.com>
2026-02-21 16:42:24 +0900

    CHORE: rename prj name

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 fn main() {
-    rescue_md_notebook_lib::run()
+    rescue_notepad_lib::run()
 }
